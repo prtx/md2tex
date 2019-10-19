c1d3aae902609dd4101853bf08eddd79cba32136
prtx <prtx.pratikshrestha@gmail.com>
2019-10-19 01:00:57 -0400

    Add module folder

---
+++ md2tex/__init__.py
@@ -0,0 +1,11 @@
+"""
+md2tex
+------
+Markdown to LaTeX converter
+"""
+
+
+__PKG_NAME__ = "md2tex"
+__version__ = "0.0.0"
+__AUTHOR__ = "Pratik Shrestha (prtx)"
+__PKG_DESC__ = "Markdown to LaTeX converter"
